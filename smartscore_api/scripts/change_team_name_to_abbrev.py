2aecbc94c15825312f966b7e5af72e71d7f3f07e
Nathan Probert <nathanprobert@rogers.com>
2025-01-17 16:07:26 -0500

    final script + new linters

--- smartscore_api/scripts/change_team_name_to_abbrev.py
+++ smartscore_api/scripts/change_team_name_to_abbrev.py
@@ -1,67 +1,114 @@
-def abbrev_to_name(abbrev) -> str:
-    map = {
-        "ANA": "Anaheim",
-        "BOSS": "Boston",
-        "BUF": "Buffalo",
-        "CAR": "Carolina",
-        "CBJ": "Columbus",
-        "CGY": "Calgary",
-        "CHI": "Chicago",
-        "COL": "Colorado",
-        "DAL": "Dallas",
-        "DET": "Detroit",
-        "EDM": "Edmonton",
-        "FLA": "Florida",
-        "LAK": "Los Angeles",
-        "MIN": "Minnesota",
-        "MTL": "Montreal",
-        "NJD": "New Jersey",
-        "NSH": "Nashville",
-        "NYI": "NY Islanders",
-    }
-    return
-
-
-def name_to_abbrev(name) -> str:
-    map = {
-        "Anaheim": "ANA",
-        "Boston": "BOSS",
-        "Buffalo": "BUF",
-        "Carolina": "CAR",
-        "Columbus": "CBJ",
-        "Calgary": "CGY",
-        "Chicago": "CHI",
-        "Colorado": "COL",
-        "Dallas": "DAL",
-        "Detroit": "DET",
-        "Edmonton": "EDM",
-        "Florida": "FLA",
-        "Los Angeles": "LAK",
-        "Minnesota": "MIN",
-        "Montreal": "MTL",
-        "New Jersey": "NJD",
-        "Nashville": "NSH",
-        "Ottawa": "OTT",
-        "Philadelphia": "PHI",
-        "Pittsburgh": "PIT",
-        "Seattle": "SEA",
-        "San Jose": "SJS",
-        "St Louis": "STL",
-        "Winnipeg": "WPG",
-        "Tampa Bay": "TBL",
-        "Toronto": "TOR",
-        "Utah": "UTA",
-        "Vancouver": "VAN",
-        "Vegas": "VGK",
-        "Washington": "WSH",
-    }
-    abbrev = map.get(name)
-    if not abbrev:
-        # which new york is this :(
-        pass
-    return
-
-
 from smartscore_api.config import collection
 
 
+def name_to_abbrev(name: str) -> str:
+  map = {
+    "Arizona": "ARI",
+    "Anaheim": "ANA",
+    "Boston": "BOS",
+    "Buffalo": "BUF",
+    "Carolina": "CAR",
+    "Columbus": "CBJ",
+    "Calgary": "CGY",
+    "Chicago": "CHI",
+    "Colorado": "COL",
+    "Dallas": "DAL",
+    "Detroit": "DET",
+    "Edmonton": "EDM",
+    "Florida": "FLA",
+    "Los Angeles": "LAK",
+    "Minnesota": "MIN",
+    "Montreal": "MTL",
+    "Montréal": "MTL",
+    "New Jersey": "NJD",
+    "Nashville": "NSH",
+    "Ottawa": "OTT",
+    "Philadelphia": "PHI",
+    "Pittsburgh": "PIT",
+    "Seattle": "SEA",
+    "San Jose": "SJS",
+    "St Louis": "STL",
+    "St. Louis": "STL",
+    "Winnipeg": "WPG",
+    "Tampa Bay": "TBL",
+    "Toronto": "TOR",
+    "Utah": "UTA",
+    "Vancouver": "VAN",
+    "Vegas": "VGK",
+    "Washington": "WSH",
+    "Islanders": "NYI",
+    "Rangers": "NYR",
+  }
+  return map.get(name.strip())
+
+
+def update_team_names():
+  dates = {}
+  dates_done = {}
+
+  count = 0
+  skipped = 0
+  cursor = collection.find()
+
+  for doc in cursor:
+    if count % 100 == 0 and count != 0:
+      print(f"Processed {count} players.")
+    if skipped % 100 == 0 and skipped != 0:
+      print(f"Skipped {skipped} players.")
+
+    team_name = doc.get("team_name")
+    player_name = doc.get("name")
+    date = doc.get("date")
+    tgpg = doc.get("tgpg")
+    otga = doc.get("otga")
+
+    # team_name is already an abbreviation, skip
+    if len(team_name) <= 3:
+      skipped += 1
+      continue
+
+    if team_name == "New York":
+      selected_team = dates.get((date, tgpg, otga))
+      if not selected_team:
+        # check if same day, because then it must be the other team
+        played_today = dates_done.get(doc.get("date"))
+        if played_today and played_today == "Islanders":
+          selected_team = "Rangers"
+        elif played_today and played_today == "Rangers":
+          selected_team = "Islanders"
+
+        else:
+          print(f"Player: {player_name}, Team: {team_name}, Date: {doc.get('date')}")
+          choice = (
+            input("Is this player on the Islanders (i) or Rangers (r)? ")
+            .strip()
+            .lower()
+          )
+          if choice == "i":
+            selected_team = "Islanders"
+
+          elif choice == "r":
+            selected_team = "Rangers"
+
+          else:
+            print("Invalid choice. Skipping...")
+            continue
+
+          print(f"Selected: {selected_team}")
+          dates[(doc.get("date"), tgpg, otga)] = selected_team
+          dates_done[doc.get("date")] = selected_team
+    else:
+      selected_team = team_name
+
+    abbrev = name_to_abbrev(selected_team)
+    if not abbrev:
+      print(f"Could not find abbreviation for team: {selected_team}")
+      print(doc)
+      exit()
+
+    collection.update_one({"_id": doc["_id"]}, {"$set": {"team_name": abbrev}})
+    count += 1
+
+
+if __name__ == "__main__":
+  update_team_names()
